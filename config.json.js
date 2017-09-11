bfeaa50ae69ee981083ab5e0a6d429d66630c18b
sadoneli <sadoneli@gmail.com>
2017-09-11 23:43:08 +0800

    update ddnsto

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "8da529bcea0ae0578646a9140781085e", 
+    "md5": "1f0136cdccd86e02630c697a05ac7eb0", 
     "module": "ddnsto", 
     "title": "DDNSTO远程控制", 
     "version": "2.0"
