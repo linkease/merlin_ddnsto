8d14a0cdbf3a303ec2739a6024e1bc439926985c
clangcn <clangcn@gmail.com>
2018-11-13 20:36:09 +0800

    修复不能显示路由器ID问题

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "375c9f7eb7520a197e0de0dbbe3278f3", 
+    "md5": "b9d2ed2343fc601f9cb1595051b5db6a", 
     "module": "ddnsto", 
     "title": "DDNSTO远程控制", 
-    "version": "2.8"
+    "version": "2.9"
 }
