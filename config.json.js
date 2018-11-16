579f23e203b8f070a4cd2860f6e550fb01b6fad7
clangcn <clangcn@gmail.com>
2018-11-16 08:25:12 +0800

    update .gitignore

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "b9d2ed2343fc601f9cb1595051b5db6a", 
+    "md5": "5f0ea785f95b00df4fa6380f54be4057", 
     "module": "ddnsto", 
     "title": "DDNSTO远程控制", 
     "version": "2.9"
