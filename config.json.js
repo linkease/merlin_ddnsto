6cd732b1f3a47a4e7174757dc4dd4b37bc2b2e14
janson <janson@gmail.com>
2022-09-26 15:27:08 +0800

    bump to 3.0.0

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "64947cb087dd4607a2f5026612f6cd97", 
+    "md5": "949612b4199497b63fe003eb29265e0e", 
     "module": "ddnsto", 
     "title": "DDNSTO远程控制", 
-    "version": "2.9.3"
+    "version": "3.0.0"
 }
